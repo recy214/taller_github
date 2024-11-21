af3334a5c35b8ed636272382bdcfd2ec5f03790d
recy214 <a2223330175@alumnos.uat.edu.mx>
2024-11-21 16:27:56 -0600

    Practica del Curso de Github

---
+++ holamundo.py
@@ -0,0 +1,6 @@
+print ("ingresa un numero")
+numero = input()
+if numero >= 100:
+    print("Hola mundo, mi primer codigo en python :3")
+else:
+    print("adios mundo, mi numero era menor al del codigo :,c")
